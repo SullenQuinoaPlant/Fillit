ad70184cf80047b6311a64a9568badd1cbe243ff
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-12 00:46:15 +0100

    incermental

--- Makefile.mk
+++ Makefile.mk
@@ -8,13 +8,18 @@ OBJS := $(patsubst %,$(OBJ)/%.o,$(TARGETS))
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
-all : $(NAME)
+all : list_objs $(NAME)
+
+.PHONY : list_objs
+list_objs :
+	@echo "objects to be built are : $(OBJS)
 
 $(NAME) : $(OBJS)
 	$(CC) -o $(NAME) $(OBJS)
 
 $(OBJ)/%.o : $(SRC)/%.c
 	$(CC) $(CFLAGS) -o $@ $<
+	ls $(OBJ)
 
 clean :
 	-rm $(OBJS)
