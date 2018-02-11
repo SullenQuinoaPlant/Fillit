64836d8a5384457c3370fa870dea550c9b72c0af
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-11 14:28:46 +0100

    incremental

--- .travis/install_cmocka.sh
+++ .travis/install_cmocka.sh
@@ -2,7 +2,6 @@
 
 git clone https://github.com/clibs/cmocka.git cmocka_download
 mkdir -p cmocka_build
-LD_LIBRARY_PATH=$(pwd)/libs
 cd cmocka_build
 cmake -DCMAKE_INSTALL_PREFIX=$LD_LIBRARY_PATH ../cmocka_download
 make
