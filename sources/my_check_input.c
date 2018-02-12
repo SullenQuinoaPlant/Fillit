5ab0b14e2647d6516e3dac050e0c42642569456a
auie <noe.42.2017@gmail.com>
2018-02-12 21:29:06 +0100

    inc

--- sources/my_check_input.c
+++ sources/my_check_input.c
@@ -3,6 +3,36 @@
 /*BUFF_SZ is 26 * 4 * 5 + 25(empty lines) + 1 (file too long)*/
 #define BUFF_SZ 546
 
+static void		copy_tmino_into_big(unsigned char (*big)[6],\
+									unsigned char (*mino)[4])
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < 6 && (j = -1))
+		while (++j < 6)
+			if (i > 0 && i < 4 && j > 0 && j > 4)
+				big[i][j] = mino[i - 1][j - 1];
+			else
+				big[i][j] = '.';
+}
+
+static int		check_last_tmino_shape(t_mino *ar, int i)
+{
+	unsigned char	big[6][6] = {0};
+	unsigned char	(*p)[4];
+
+	copy_tmino_into_big(big, ar[i].ar);
+	p = big[1][1];
+	i = -1;
+	while (++i < 4 && (j = -1))
+		while (++j < 4)
+			if (p[i][j])
+			{
+				
+
+}
 
 static int		set_ret_ar(t_mino *ar, const char *input)
 {
@@ -21,7 +51,8 @@ static int		set_ret_ar(t_mino *ar, const char *input)
 			k = -1;
 			while (++k < 4 && ((c = *input++) == '.' || c == '#'))
 				ar[i].ar[j][k] = c == '.' ? c : 'A' + i;
-			if (k < 4 || (c = *input++) ^ '\n')
+			if (k < 4 || (c = *input++) ^ '\n' ||\
+				check_last_tmino_shape(ar, i))
 				return (1);	
 		}
 		c = *input++;
@@ -40,17 +71,13 @@ static t_mino	*get_input(const char *input)
 	if ((fd = open(input, O_RDONLY) ^ -1))
 	{
 		if((index = read(fd, buff, BUFF_SZ)) > 0 && index < BUFF_SZ)
-		{
 			if ((ret_ar = malloc(26 * sizeof(t_mino))))
-			{
 				if (set_ret_ar(ret_ar, buff))
 				{
 					free(ret_ar);
 					ret_ar = 0;
 					my_usage(USAGE_BAD_TETRAMINOS);
 				}
-			}
-		}
 		close(index);
 	}
 	else
