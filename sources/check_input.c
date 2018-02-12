23ad1e5f5c7611606bc724b7f7e505674fe69bc2
auie <noe.42.2017@gmail.com>
2018-02-12 14:06:07 +0100

    inc

--- sources/check_input.c
+++ sources/check_input.c
@@ -1,7 +1,45 @@
 #include "h.h"
 
-int		check_input(const char *input)
+/*BUFF_SZ is 26 * 4 * 5 + 25(empty lines) + 1 (file too long)*/
+#define BUFF_SZ 546
+
+int		set_ret_ar(t_mino *ar, const char *input)
 {
-	(void)input;
+	return (0);
+}
+
+t_mino	*get_input(const char *input)
+{
+	int		fd;
+	char	buff[BUFF_SZ] = {0};
+	ssize_t	index;
+	t_mino	*ret_ar;
+
+	ret_ar = 0;
+	if ((fd = open(input, O_RDONLY) ^ -1))
+	{
+		if((index = read(fd, buff, BUFF_SZ)) > 0 && index < BUFF_SZ)
+		{
+			if ((ret_ar = malloc(26 * sizeof(t_mino))))
+			{
+				if (set_ret_ar(ret_ar, buff))
+				{
+					free(ret_ar);
+					ret_ar = 0;
+					usage();
+				}
+			}
+		}
+		close(index);
+	}
+	return (ret_ar);
+}
+
+t_mino	*check_input(int ac, char *av[])
+{
+	if (ac ^ 2)
+		usage();
+	else
+		return (get_input(av[1]));
 	return (0);
 }
--- sources/h.h
+++ sources/h.h
@@ -1,6 +1,12 @@
 #ifndef H_H
 # define H_H
 
+# include <unistd.h>
+# include <fcntl.h>
 
+typedef struct	s_tetramino {
+	unsigned char	ar[4][4];
+
+}				t_mino;
 
 #endif
