49b769e6f9d76f19724fe36054fe038a23f8d482
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-13 14:27:00 +0100

    inc

--- sources/fillit.h
+++ sources/fillit.h
@@ -18,6 +18,15 @@ typedef struct	s_tetramino {
 # define USAGE_BAD_FILENAME 2
 # define USAGE_BAD_TETRAMINOS 3
 
+/*t_mino stack maximum dimensions :*/
+/*	high upper limit (could be reduced)
+ *	assuming one square block 4 * 4 
+ *	per t_mino*/
+# define MAX_STACK_WIDTH 24
+# define MAX_STACK_HEIGHT 20
+
+typedef char[MAX_STACK_HEIGHT][MAX_STACK_WIDTH] t_stack_grid;
+
 /*functions*/
 void	my_arrange_tminos_arr(t_mino *arr);
 t_mino	*my_check_input(int ac, char *av[]);
