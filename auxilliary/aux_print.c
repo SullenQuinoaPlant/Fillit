c09043c7d5526143b4a9d76fc9298dcdad0eb5a7
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-14 13:05:03 +0100

    incremental

--- auxilliary/aux_print.c
+++ auxilliary/aux_print.c
@@ -10,18 +10,18 @@ void	aux_print_2d_char_arr(char *arr, int d1, int d2)
 	while (++i < d1 && (j = -1))
 	{
 		while (++j < d2)
-			printf("\t%d", arr[i * d2 + j]);
+			printf("\t%c", arr[i * d2 + j]);
 		printf("\n");
 	}
 }
 
 void	aux_print_tmino(t_mino print_this)
 {
-	aux_print_2d_char_arr((int*)print_this.ar, 4, 4);
+	aux_print_2d_char_arr((char*)print_this.ar, 4, 4);
 }
 
-void	aux_print_tmino_string(t_mino *string)
+void	aux_print_tmino_string(t_mino *str)
 {
-	while ((*string).ar[0][0])
-		aux_print_tmino(*string++);
+	while ((*str).ar[0][0])
+		aux_print_tmino(*str++);
 }
