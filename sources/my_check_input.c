2b8a05d44f0a41a7d38fddd6905b0585856539bb
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-02-14 17:46:37 +0100

    incremental

--- sources/my_check_input.c
+++ sources/my_check_input.c
@@ -106,12 +106,9 @@ static t_mino	*get_input(const char *input)
 
 t_mino			*my_check_input(int ac, char *av[])
 {
-	(void)ac;
-	if ((ac = open(av[1], O_RDONLY) != -1))
-		printf("received string : %s\n", av[1]);
-//	if (ac ^ 2)
-//		my_usage(USAGE_ARG_COUNT);
-//	else
-//		return (get_input(av[1]));
+	if (ac ^ 2)
+		my_usage(USAGE_ARG_COUNT);
+	else
+		return (get_input(av[1]));
 	return (0);
 }
